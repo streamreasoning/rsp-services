8a1f24aeaa54233cbd42e46a1b6946c3b2c9a8a2
Marco Balduini <balduini.marco@gmail.com>
2013-11-14 17:38:39 +0100

    - Observer Interface added - dependencies versions minor changes

---
+++ src/main/java/org/streamreasoning/rsp_services/interfaces/Continuous_Query_Observer_Interface.java
@@ -0,0 +1,8 @@
+package org.streamreasoning.rsp_services.interfaces;
+
+import java.util.Observer;
+
+
+public interface Continuous_Query_Observer_Interface extends Observer{
+
+}
--- src/main/java/org/streamreasoning/rsp_services/interfaces/Observer_Interface.java
+++
@@ -1,8 +0,0 @@
-package org.streamreasoning.rsp_services.interfaces;
-
-
-public interface Observer_Interface{
-
-	public void update(Object arg0, Object arg1);
-
-}
